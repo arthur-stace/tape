ae7b70096c60363ae7e09e15337127e78c23cfa5
Arthur Stace <arthur.stace@pm.me>
2020-07-12 09:05:18 +1000

    test.mk

--- test.mk
+++ test.mk
@@ -1,3 +1,27 @@
-.PHONY: $(NAME)
-user: $(NAME)
-	docker exec -it $(CONTAINER) create_user $?
+default: test
+
+
+
+test: backup snapshots find
+
+
+init:
+	@docker-compose --file docker-compose.test.yml run init
+
+
+
+snapshots:
+	@docker-compose --file docker-compose.test.yml run snapshots
+
+
+
+find:
+	@docker-compose --file docker-compose.test.yml run find
+
+
+
+backup:
+	@docker-compose --file docker-compose.test.yml run backup
+
+
+
