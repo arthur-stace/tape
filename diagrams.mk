af9e8cde5d28f356ee225ad944129c3c4167ff9a
Arthur Stace <arthur.stace@pm.me>
2020-07-14 11:30:28 +1000

    diagrams.mk

--- diagrams.mk
+++ diagrams.mk
@@ -4,5 +4,12 @@ diagrams:
 
 DIAGRAMS = $(wildcard docs/**/*.png)
 
+docs/%/*.uml:
+	plantuml -tpng $@
+	echo `dirname $@`
+
 README.md: diagrams
 	for item in $(DIAGRAMS); do echo "![`dirname $$item`](./$$item)" >> $@; done
+
+clean:
+	rm -rf docs/**/*.png
